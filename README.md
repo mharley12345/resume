9ec938823b27c8ac5dc6e4fd9e8f619235d6c772
Mike Harley <mharley12345@gmail.com>
2020-01-10 10:59:01 -0800

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# resumetemplate
+An online resume template in reactjs  
+Instruction to run:-  
+1)Clone the repo  
+
+2)Edit the resumeData.js file in src folder  
+
+3)Generate build using command:  
+ ```npm run build```
+
---
+++ src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+  "imagebaseurl":"https://avatars2.githubusercontent.com/u/47269554?s=460&v=4",
+  "name": "Michael Harley",
+  "role": "Full Stack Developer",
+  "linkedinId":"michael-harley-5336a9188",
+  "skypeid": "live:.cid.ba5abbcab6b81b1f",
+  "roleDescription": "I like dabbling in various parts of  development and love to learn about new technologies, create secure and developer friendly apis and servers. ",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/michael-harley-5336a9188/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/mharley12345",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com/live:.cid.ba5abbcab6b81b1f",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I have 3 years as a freelance developer. I recently decided to advance my education by attending Lambda School.",
+  "address":"Auburn,Wa",
+  "website":"http://info.mike-harley.tech",
+  "education":[
+    {
+      "UniversityName":"Lambda School",
+      "specialization":"Full Stack Development",
+      "MonthOfPassing":"Mar",
+      "YearOfPassing":"2020",
+      "Achievements":"Some Achievements"
+    },
+
+  ],
+  "work":[
+    {
+      "CompanyName":"Self Employed",
+      "specialization":"Website and Application Developer",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2020",
+     
+    },
+    {
+      "CompanyName":"Lambda School",
+      "specialization":"Student / Intern",
+      "MonthOfLeaving":"Still",
+      "YearOfLeaving":"Attending",
+     
+    }
+  ],
+  "skillsDescription":"Some Of My Skills Include",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"Node"
+    },
+    {
+      "skillname":"React"
+    },
+    {
+      "skillname":"PostgreSQL"
+    },
+    {
+      "skillname":"AWS s3"
+    }
+  ],
+
+  "testimonials":[
+    {
+      "description":"Mike did a wonderful job on our small ecommerce site. He is a pleasure to work with",
+      "name":"Frank Johnson  Camping Thread Inc."
+    },
+    {
+      "description":"Mike did my site for me about 2 months ago and since it has been up. My business has doubled. Awesome Job!!!!",
+      "name":"L.Shep Mcc#2.com"
+    }
+  ]
+}
+
+export default resumeData
